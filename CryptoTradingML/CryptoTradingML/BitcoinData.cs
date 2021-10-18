924f17c9a194531909a41b2d8c223bed6e1a5a91
Nathan Lewis <njl17b@acu.edu>
2021-10-18 09:25:29 -0500

    Added required packages to project folder.

--- CryptoTradingML/CryptoTradingML/BitcoinData.cs
+++ CryptoTradingML/CryptoTradingML/BitcoinData.cs
@@ -1,1 +1,11 @@
-//Bitcoin Data
+using System;
+using Microsoft.ML.Data;
+
+namespace CryptoTradingML
+{
+    public class BitcoinData
+    {
+        [LoadColumn(0)]
+        public string Time { get; set; }
+    }
+}
