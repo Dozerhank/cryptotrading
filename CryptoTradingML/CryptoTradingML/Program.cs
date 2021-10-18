eb31e6d9efb588b7053c9deedd11cfb2bf67b871
Nathan Lewis <njl17b@acu.edu>
2021-10-17 20:30:46 -0500

    Testing Git

--- CryptoTradingML/CryptoTradingML/Program.cs
+++ CryptoTradingML/CryptoTradingML/Program.cs
@@ -6,7 +6,7 @@ namespace CryptoTradingML
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World! But edited");
         }
     }
 }
