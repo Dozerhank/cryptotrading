d4e0bbd442e3d3d632d5b145a1e5d1c07e8c2544
Dozerhank <65521931+Dozerhank@users.noreply.github.com>
2021-10-17 20:18:30 -0500

    Add files via upload

---
+++ CryptoTradingML/CryptoTradingML/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CryptoTradingML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
