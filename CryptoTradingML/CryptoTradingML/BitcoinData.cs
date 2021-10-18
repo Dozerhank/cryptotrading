0054f01fd7c59634de6a3301217707bdb4c02795
Nathan Lewis <njl17b@acu.edu>
2021-10-18 13:04:03 -0500

    created data pipeline path

--- CryptoTradingML/CryptoTradingML/BitcoinData.cs
+++ CryptoTradingML/CryptoTradingML/BitcoinData.cs
@@ -7,5 +7,17 @@ namespace CryptoTradingML
     {
         [LoadColumn(0)]
         public string Time { get; set; }
+
+        [LoadColumn(1)]
+        public string Date { get; set; }
+
+        [LoadColumn(2)]
+        public float PrevBitcoinPrice { get; set; }
+
+        [LoadColumn(3)]
+        public float BitcoinPrice { get; set; }
+
+        [LoadColumn(4)]
+        public float PriceChange { get; set; }
     }
 }
--- CryptoTradingML/CryptoTradingML/Program.cs
+++ CryptoTradingML/CryptoTradingML/Program.cs
@@ -1,12 +1,22 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Microsoft.ML;
+using Microsoft.ML.Data;
+using static Microsoft.ML.DataOperationsCatalog;
+using Microsoft.ML.Trainers;
+using Microsoft.ML.Transforms.Text;
 
 namespace CryptoTradingML
 {
     class Program
     {
+        static readonly string dataPath = Path.Combine(Environment.CurrentDirectory, "Data", "BitcoinData.txt");
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World! But edited");
+            MLContext mlcontext = new MLContext();
         }
     }
 }
