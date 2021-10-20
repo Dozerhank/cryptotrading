49ff93c09f7ea5333c5bf496e43b64be5fac4b1b
Nathan Lewis <njl17b@acu.edu>
2021-10-20 09:38:49 -0500

    Added DataPreparation file

---
+++ CryptoTradingML/CryptoTradingML/DataPreparation.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CryptoTradingML
+{
+    class DataPreparation
+    {
+        string testDataFile { get; set; }
+        string newDataFile { get; set; }
+
+        public void prepareData()
+        {
+
+        }
+
+        public void addData(string d)
+        {
+
+        }
+
+        public string getData(int column, int row)
+        {
+            string result = "";
+
+            return result;
+        }
+    }
+}
